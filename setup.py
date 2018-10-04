8423bca7eee219303860e48874a0c45dbbfda724
John Ramsden <johnramsden@riseup.net>
2018-10-04 16:15:16 -0700

    Remove git version function from setup

--- setup.py
+++ setup.py
@@ -21,36 +21,9 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
-
-def vcs_release(version: str):
-    """
-    If in git repo, get git version
-    """
-    try:
-        git_hash = subprocess.check_output(
-                        ['git', 'rev-parse', '--short', 'HEAD'],
-                        universal_newlines=True, stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError:
-        return version
-
-    return version + '+git.' + git_hash
-
-
-def get_release_version(version: str):
-    has_tags = None
-    try:
-        has_tags = subprocess.check_output(
-                                    ['git', 'tag', '-l', '--points-at', 'HEAD'],
-                                    universal_newlines=True, stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError:
-        pass
-
-    return version if has_tags and has_tags.startswith("v") else vcs_release(version)
-
-
 setup(
     name='pyzfscmds',
-    version=get_release_version(__version__),
+    version=__version__,
     description='ZFS CLI Command Wrapper Library',
     long_description=readme(),
     url='http://github.com/johnramsden/pyzfscmds',
@@ -58,7 +31,7 @@ setup(
     author_email='johnramsden@riseup.net',
     license='BSD-3-Clause',
     classifiers=[
-      'Development Status :: 3 - Alpha',
+      'Development Status :: 3 - Beta',
       'License :: OSI Approved :: BSD License',
       'Programming Language :: Python :: 3.6',
     ],
